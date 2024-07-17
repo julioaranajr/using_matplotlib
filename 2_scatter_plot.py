e86e0c7fee7b2034751a4171a4e470f31abd3afe
Julio Arana Jr. <julioarana.de@gmail.com>
2024-07-17 19:37:25 +0200

    scatter plot demo

---
+++ 2_scatter_plot.py
@@ -0,0 +1,8 @@
+import matplotlib
+# Import the pyplot module
+import matplotlib.pyplot as plt
+
+# Create a scatter plot
+plt.scatter([1, 2, 3, 4, 5], [1, 4, 9, 16, 25])
+# Save the plot as a PNG file
+plt.savefig('results/scatter_plot.png')
